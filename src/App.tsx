bfcf4b1f72b3dc1c3ef40095734ca768b9faa798
Zakyaru <arseni.ergin@gmail.com>
2025-11-23 19:44:09 +0100

    test de règle PR

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-4xl font-bold text-white">
-        Mon futur site vitrine ✨
+        Mon futur site vitrine et plus✨
       </h1>
     </div>
   );
