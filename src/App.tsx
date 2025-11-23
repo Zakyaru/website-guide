2fe0247a42e49bc5675d866db60c2c253916d60e
Zakyaru <arseni.ergin@gmail.com>
2025-11-23 19:01:01 +0100

    Initial commit - Vite React - Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        Mon futur site vitrine ✨
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
