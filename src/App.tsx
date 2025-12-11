bf774034a83462e2c4b23683ea3f08796f1e5a98
Zakyaru <arseni.ergin@gmail.com>
2025-12-11 21:21:54 +0100

    initialisation des styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 function App() {
+  //ici qu'on fait le routing normalement
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-white">
-        Mon futur site vitrine ✨
-      </h1>
+    <div className="container-main text-main bg-gray-100">
+      <div className="min-h-screen flex flex-col gap-2 items-center justify-center ">
+        <h1 className="text-4xl font-bold">
+          Mon futur site vitrine ✨
+        </h1>
+        <p>Et là maintenant j'écris un texte super long pour qu'il prenne tout l'écran et carrément plus parce que là je vois que c'est pas assez. Phrase finale pour arriver sur un saut de ligne et s'envoler dans l'espace avec une fusée arc-en-ciel qui vole vraiment haut dans les nuages et qui part dans l'espace un peu comme le fond défaut de ce site, mais ça ne restera pas longtemps comme ça. Oh non Mme Bovary ne l'acccepterai pas.</p>
+        <button className="btn btn-primary"> Button </button>
+        <button className="btn btn-secondary"> Button </button>
+      </div>
     </div>
+    
   );
 }
 
