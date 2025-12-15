b90a92787233890028a60613363ed9bd8d864dba
Zakyaru <arseni.ergin@gmail.com>
2025-12-15 20:34:54 +0100

    développement fini de Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import Hero from "./components/Hero.tsx";
+
 function App() {
   //ici qu'on fait le routing normalement
   return (
-    <div className="container-main text-main bg-gray-100">
-      <div className="min-h-screen flex flex-col gap-2 items-center justify-center ">
-        <h1 className="text-4xl font-bold">
-          Mon futur site vitrine ✨
-        </h1>
+    <div className="container-main min-h-screen text-main bg-gray-100">
+      <Hero></Hero>
+      <div className="flex flex-col gap-2 items-center justify-center ">
+        <h2>
+          Visites proposées
+        </h2>
         <p>Et là maintenant j'écris un texte super long pour qu'il prenne tout l'écran et carrément plus parce que là je vois que c'est pas assez. Phrase finale pour arriver sur un saut de ligne et s'envoler dans l'espace avec une fusée arc-en-ciel qui vole vraiment haut dans les nuages et qui part dans l'espace un peu comme le fond défaut de ce site, mais ça ne restera pas longtemps comme ça. Oh non Mme Bovary ne l'acccepterai pas.</p>
-        <button className="btn btn-primary"> Button </button>
-        <button className="btn btn-secondary"> Button </button>
       </div>
     </div>
     
